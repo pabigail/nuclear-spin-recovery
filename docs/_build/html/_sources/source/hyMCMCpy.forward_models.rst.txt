f4559218c5fe3bcd495909d4da0bb0c93263620a
Abby Poteshman <78704917+pabigail@users.noreply.github.com>
2025-03-26 15:52:17 -0500

    added param class

--- docs/_build/html/_sources/source/hyMCMCpy.forward_models.rst.txt
+++ docs/_build/html/_sources/source/hyMCMCpy.forward_models.rst.txt
@@ -1,6 +1,17 @@
 hyMCMCpy.forward\_models package
 ================================
 
+Submodules
+----------
+
+hyMCMCpy.forward\_models.forward\_models module
+-----------------------------------------------
+
+.. automodule:: hyMCMCpy.forward_models.forward_models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
 Module contents
 ---------------
 
---
+++ hyMCMCpy/forward_models/forward_models.py
@@ -0,0 +1,21 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from scipy.stats import poisson, nbinom
+
+class ForwardModel(ABC):
+    """
+    This abstract class is takes in Params and other arguments to return data-like output. 
+    Should not be initialized directly (use an implemented subclass)
+
+    Args:
+        params (Params): the parameter that seek to recover using MCMC methods
+        **kwargs (dict): specific parameters that are unchanging across instatiations
+
+    """
+    def __init__(self, params, **kwargs):
+        self.params = params
+
+    @abstractmethod
+    def forward_model(self):
+        pass
+

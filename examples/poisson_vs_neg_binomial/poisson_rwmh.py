e551711b0f7dff671f5e09bd272dd8c33674ff32
Abby Poteshman <78704917+pabigail@users.noreply.github.com>
2025-03-31 17:51:03 -0400

    rwmh for poisson one alg example

--- examples/poisson_vs_neg_binomial/poisson_rwmh.py
+++ examples/poisson_vs_neg_binomial/poisson_rwmh.py
@@ -10,6 +10,7 @@ from hymcmcpy.params import Params
 from hymcmcpy.forward_models import PoissonForwardModel
 from hymcmcpy.likelihood_models import PoissonLogLikelihood
 from hymcmcpy.mcmc_algorithms.steps import RWMHContinuousStep
+from hymcmcpy.mcmc_algorithms.runners import MCMCRunner, MCMCIterations
 
 # Generate data
 num_data = 10000
@@ -17,25 +18,50 @@ lambda_param = 2.5
 poisson_data = np.random.poisson(lambda_param, num_data)
 
 # Initialize Params with example data
-params = Params(
+initial_params = Params(
     names=['lambda'],
     vals=[2.6], # initial value
     discrete=[False]
 )
 
+
+
+class PoissonMCMCRunner(MCMCRunner):
+    def instantiation_step(self, algorithm, **kwargs):
+        return RWMHContinuousStep(self.data, self.likelihood_model, **kwargs)
+
+
+
 # Initialize Forward model with example params
-forward_model_poisson = PoissonForwardModel(params)
-print("forward model:", forward_model_poisson.compute(k=10))
+forward_model_poisson = PoissonForwardModel(initial_params)
+# print("forward model:", forward_model_poisson.compute(k=10))
 
 
 # Initialize PoissonLogLikelhihood model
 poisson_log_likelihood = PoissonLogLikelihood(forward_model_poisson,
                                               poisson_data)
 
-print("log likelihood:", poisson_log_likelihood.log_likelihood())
+# print("log likelihood:", poisson_log_likelihood.log_likelihood())
+
+total_iterations = 10
+sigma_sq = 0.5
+
+schedule = [(RWMHContinuousStep, total_iterations)]
+
+runner = PoissonMCMCRunner(poisson_data,
+                           poisson_log_likelihood,
+                           schedule,
+                           total_iterations,
+                           initial_params=initial_params,
+                           sigma_sq=sigma_sq)
+
+mcmc_results = runner.run()
+
+print(mcmc_results)
 
 
 
+'''
 # Take one step of RWMH
 sigma_sq = 1
 rwmh_step = RWMHContinuousStep(poisson_data,
@@ -64,4 +90,4 @@ print("Updated parameters:")
 print(third_params)
 
 print("accept? 2", accept_2)
-
+'''
--- hyMCMCpy/mcmc_algorithms/runners.py
+++ hyMCMCpy/mcmc_algorithms/runners.py
@@ -1,7 +1,9 @@
 import numpy as np
+from abc import ABC, abstractmethod
 from hymcmcpy.params import Params
 
-class MCMC_iterations(np.ndarray):
+
+class MCMCIterations(np.ndarray):
     """
     A structured NumPy array to store parameter values at different iterations of a hybrid MCMC walker.
 
@@ -47,3 +49,84 @@ class MCMC_iterations(np.ndarray):
         obj['params'] = params
 
         return obj
+
+
+class MCMCRunner(ABC):
+    """
+    Abstract base class for running a hybrid MCMC algorithm given an initial parameter set,
+    a sequence of algorithms, and a total number of iterations.
+
+    Args:
+        data (array-like): Observed data against which proposed parameters and forward model will be compared.
+        likelihood_model (LikelihoodModel): A likelihood model used to evaluate the likelihood of the observed data.
+        initial_params (Params): The initial parameters for the MCMC run.
+        schedule (list of tuples): A list of (MCMCStep algorithm, int num_iterations) tuples defining the
+            sequence of algorithms and their respective iteration counts.
+        total_iterations (int): The total number of MCMC iterations to perform.
+        existing_iterations (MCMC_iterations, optional): An existing MCMC_iterations object to append new results.
+
+    Returns:
+        MCMC_iterations: A structured array storing MCMC step details.
+    """
+    def __init__(self, data, likelihood_model, schedule, total_iterations, initial_params=None, existing_iterations=None,  **kwargs):
+        if initial_params is None and existing_iterations is None:
+            raise ValueError("Either intial_params or existing_iterations must be provided")
+        if initial_params is not None and existing_iterations is not None:
+            raise ValueError("Only one of intial_params or existing_iterations can be provided")
+        
+        self.data = data
+        self.likelihood_model = likelihood_model
+        self.initial_params = initial_params
+        self.schedule = schedule
+        self.total_iterations = total_iterations
+        self.existing_iterations = existing_iterations
+        self.kwargs = kwargs
+    
+    @abstractmethod
+    def instantiation_step(self, algorithm, **kwargs):
+        """
+        Instantiate an MCMCStep subclass based on the given algorithm nickname and additional parameters.
+
+        Args:
+            algorithm_nickname (str): The nickname of the MCMC algorithm to instantiate.
+            **kwargs: Additional parameters required for the specific MCMC algorithm.
+
+        Returns:
+            MCMCStep: An instance of the corresponding MCMC algorithm.
+        """
+        pass
+
+
+    def run(self):
+        """
+        Execute the MCMC sequence as per the defined schedule.
+
+        Returns:
+            MCMC_iterations: A structured array storing the params, whether accepted, 
+            and the algorithm at each step of the MCMC iterations.
+        """
+        current_params = self.initial_params if self.initial_params is not None else self.existing_iterations['params'][-1]
+        algorithms = []
+        accepts = []
+        params_list = []
+
+        if self.existing_iterations:
+            algorithms.extend(self.existing_iterations['algorithm'])
+            accepts.extend(self.existing_iterations['accept'])
+            params_list.extend(self.existing_iterations['params'])
+
+        for algorithm, num_iter in self.schedule:
+            step = self.instantiation_step(algorithm, **self.kwargs)
+            
+            for _ in range(num_iter):
+                new_params, accepted = step.one_step(current_params)
+                algorithms.append(algorithm.nickname)
+                accepts.append(accepted)
+                params_list.append(new_params)
+                
+                if accepted:
+                    current_params = new_params
+
+        return MCMCIterations(algorithms, accepts, params_list)
+
+

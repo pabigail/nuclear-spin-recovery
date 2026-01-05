03227108b37bda04a05e2ebc3dfd55e813c54d4f
Abby Poteshman <78704917+pabigail@users.noreply.github.com>
2026-01-05 11:50:17 -0500

    started post_init for SingleExperiment

--- nuclear_spin_recover/experiment.py
+++ nuclear_spin_recover/experiment.py
@@ -12,6 +12,15 @@ class SingleExperiment:
     pulses: PulseSequence
     lambda_decoherence: numbers.Real = 1.0
 
+    def __post__init(self):
+        if not isinstance(self.mag_field, numbers.Real):
+            raise(TypeError, "Magnetic field must be a real number")
+        if self.mag_field < 0:
+            raise(ValueError, "Magnetic field must be >= 0")
+        if not isinstance(self.lambda_decoherence, numbers.Real):
+            raise(TypeError, "Lambda decoherence must be a real number")
+        if self.lambda_decoherence <= 0 or self.lambda_decoherence > 1.0:
+            raise(ValueError, "Lambda decoherence must be in (0.0, 1.0]")
 
 
 @dataclass

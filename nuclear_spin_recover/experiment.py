00b4d332e9ba1ede3d6c56f81120d22a3df3490c
Abby Poteshman <78704917+pabigail@users.noreply.github.com>
2026-01-05 11:21:23 -0500

    updated pytest experiment, added default value for lambda

--- nuclear_spin_recover/experiment.py
+++ nuclear_spin_recover/experiment.py
@@ -1,16 +1,17 @@
 import numpy as np
 from dataclasses import dataclass
 from typing import List, Tuple, Dict
+from .pulse import Pulse, PulseSequence
+import numbers
 
 @dataclass
 class SingleExperiment:
     experiment_name: str
     tau: np.ndarray
     mag_field: float
-    pulses: dict
+    pulses: PulseSequence
+    lambda_decoherence: numbers.Real = 1.0
 
-    def signature(self) -> tuple:
-        return (self.experiment_name, tuple(self.tau), self.mag_field, tuple(self.pulses.items()))
 
 
 @dataclass
